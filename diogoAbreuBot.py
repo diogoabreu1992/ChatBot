1b40a6543aa4c5f96e2e6ddcc4ee10b9e47aa9af
diogoabreu1992 <diogo.abreu1992@gmail.com>
2021-10-21 00:59:05 -0400

    Update diogoAbreuBot.py

--- diogoAbreuBot.py
+++ diogoAbreuBot.py
@@ -95,7 +95,7 @@ def done(update: Update, context: CallbackContext) -> int:
         auth_token = "7452730d9a49d1fc9634fd71b9ca7921"
         client = Client(account_sid,auth_token)
         mensagem = client.messages.create(from_="+12184439389",body="Sua consulta foi marcada com Sucesso.",to=user_data.get("Numero"))
-        print(mensagem_sid)
+        print("diogo")
         update.message.reply_text(f'Novo Agendamento{mensagem.sid}: clique aqui -> /start')
 
     user_data.clear()
